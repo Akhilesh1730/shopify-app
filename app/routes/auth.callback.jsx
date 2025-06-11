aaf40e659a50ec115227a199d3670d2a2cf7226f
Akhilesh <dhoreakhilesh@gmail.com>
2025-06-11 20:01:40 +0530

    new callback commit

--- app/routes/auth.callback.jsx
+++ app/routes/auth.callback.jsx
@@ -4,9 +4,8 @@ import { authenticate } from "~/shopify.server";
 export async function loader({ request }) {
   const { session, admin } = await authenticate.admin(request);
 
+  
   console.log("✅ OAuth Success - Shop:", session.shop);
   console.log("✅ Access Token:", session.accessToken);
-
-  // ✅ Custom redirect after login
-  return redirect(`https://app.shipdartexpress.com`);
+  return redirect("https://app.shipdartexpress.com");
 }
