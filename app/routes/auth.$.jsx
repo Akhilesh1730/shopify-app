b121ff6cc4fa6e0a3767b87df4114d59b0951964
Akhilesh <dhoreakhilesh@gmail.com>
2025-06-11 20:19:07 +0530

    new callback commit 3

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -3,5 +3,5 @@ import { authenticate } from "../shopify.server";
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
 
-  return null;
+  return redirect("https://app.shipdartexpress.com");
 };
--- app/routes/auth.callback.jsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "@remix-run/node";
-import { authenticate } from "~/shopify.server";
-
-export async function loader({ request }) {
-  const { session, admin } = await authenticate.admin(request);
-  
-  return redirect("https://app.shipdartexpress.com");
-}
