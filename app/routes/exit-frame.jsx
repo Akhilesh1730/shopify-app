eeae78f0751925ea33f2effcd197e2efb94b4168
Akhilesh <dhoreakhilesh@gmail.com>
2025-06-13 02:27:10 +0530

    callback exrror added 40

--- app/routes/exit-frame.jsx
+++ app/routes/exit-frame.jsx
@@ -1,6 +1,1 @@
-import createApp from '@shopify/app-bridge';
-import {Redirect} from '@shopify/app-bridge/actions';
-
-const app = createApp(config);
-
-const redirect = Redirect.create(app);
+import {useNavigate} from '@shopify/app-bridge-react';
