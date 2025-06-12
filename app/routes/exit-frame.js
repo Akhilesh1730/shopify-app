b9495b5ee4fa9738a81a9fa45ceb3498806ede2c
Akhilesh <dhoreakhilesh@gmail.com>
2025-06-13 03:05:19 +0530

    callback exrror added 50

--- app/routes/exit-frame.js
+++ app/routes/exit-frame.js
@@ -1,13 +1,7 @@
-import createApp from '@shopify/app-bridge/development';
-import { Redirect } from '@shopify/app-bridge/actions/index.js';
+import createApp from '@shopify/app-bridge';
+import {Redirect} from '@shopify/app-bridge/actions';
 
-const app = createApp({
-  apiKey: '505015e6892ff21b6c433b09af1c7038',
-  host: new URLSearchParams(location.search).get('host'),
-  forceRedirect: true,
-});
+const app = createApp(config);
 
 const redirect = Redirect.create(app);
-
-// Redirect to a custom URL
-redirect.dispatch(Redirect.Action.REMOTE, 'https://app.shipdartexpress.com');
+redirect.dispatch(Redirect.Action.REMOTE, 'http://app.shipdartexpress.com');
