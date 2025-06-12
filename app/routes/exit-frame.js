459a256e7b54954b49a328e6fb4114503a8cbb0e
Akhilesh <dhoreakhilesh@gmail.com>
2025-06-13 03:28:12 +0530

    callback exrror added 54

--- app/routes/exit-frame.js
+++ app/routes/exit-frame.js
@@ -1,12 +1,21 @@
-import createApp from '@shopify/app-bridge';
-import {Redirect} from '@shopify/app-bridge/actions/index.js';
+import { useEffect } from "react";
+import createApp from "@shopify/app-bridge";
+import { Redirect } from "@shopify/app-bridge/actions";
 
-const config = {
-  apiKey: "505015e6892ff21b6c433b09af1c7038",
-  host: new URLSearchParams(window.location.search).get("host"),
-  forceRedirect: true
-};
-const app = createApp(config);
+export default function AppRedirect() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const config = {
+        apiKey: "505015e6892ff21b6c433b09af1c7038",
+        host: new URLSearchParams(window.location.search).get("host"),
+        forceRedirect: true,
+      };
 
-const redirect = Redirect.create(app);
-redirect.dispatch(Redirect.Action.REMOTE, 'http://app.shipdartexpress.com');
+      const app = createApp(config);
+      const redirect = Redirect.create(app);
+      redirect.dispatch(Redirect.Action.REMOTE, "http://app.shipdartexpress.com");
+    }
+  }, []);
+
+  return null;
+}
