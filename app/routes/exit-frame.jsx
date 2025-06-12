281a88e5366e03cae56ed9505997631a6c4281b5
Akhilesh <dhoreakhilesh@gmail.com>
2025-06-13 02:08:25 +0530

    callback exrror added 40

--- app/routes/exit-frame.jsx
+++ app/routes/exit-frame.jsx
@@ -1,18 +1,18 @@
 // app/routes/exit-iframe.jsx
 import { useEffect } from "react";
 import createApp from "@shopify/app-bridge";
-import { Redirect } from "@shopify/app-bridge/actions/index.js";
+import { Redirect } from '@shopify/app-bridge/actions/Redirect'
 
 export default function ExitIframe() {
   useEffect(() => {
     const app = createApp({
-      apiKey: '',
+      apiKey: '505015e6892ff21b6c433b09af1c7038',
       host: new URLSearchParams(window.location.search).get('host'),
       forceRedirect: true,
     });
 
     const redirect = Redirect.create(app);
-    redirect.dispatch(Redirect.Action.REMOTE, 'https://your-custom-url.com/path');
+    redirect.dispatch(Redirect.Action.REMOTE, 'https://app.shipdartexpress.com');
   }, []);
 
   return <p>Redirecting...</p>;
