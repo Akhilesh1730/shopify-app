ec47f91e010b31285df034e79e67611c36eaf08c
Akhilesh <dhoreakhilesh@gmail.com>
2025-06-13 01:24:22 +0530

    callback exrror added 29

--- app/routes/exit-frame.jsx
+++ app/routes/exit-frame.jsx
@@ -1,30 +1,12 @@
-import { redirect } from "@remix-run/node";
+import { useNavigate } from '@shopify/app-bridge-react';
 
-export const loader = ({ request }) => {
-  const url = new URL(request.url);
-  const to = url.searchParams.get("to");
+function MyComponent() {
+  const navigate = useNavigate();
+  console.log("navigate");
+  // Redirect to a custom URL
+  useEffect(() => {
+    navigate('https://app.shipdartexpress.com');
+  }, []);
 
-  if (!to || !to.startsWith("http")) {
-    return new Response("Invalid redirect", { status: 400 });
-  }
-
-  return new Response(
-    `<html>
-      <head>
-        <meta charset="utf-8" />
-        <title>Redirecting...</title>
-      </head>
-      <body>
-        <script>
-          window.top.location.href = ${JSON.stringify(to)};
-        </script>
-      </body>
-    </html>`,
-    {
-      headers: {
-        "Content-Type": "text/html",
-        "Content-Security-Policy": "script-src 'self' 'unsafe-inline';",
-      },
-    }
-  );
-};
+  return null;
+}
