d1da55d80dbcfb76b72bf5f97cdc29ef27a7dc3d
Akhilesh <dhoreakhilesh@gmail.com>
2025-06-13 03:13:07 +0530

    callback exrror added 52

--- app/routes/exit-frame.js
+++ app/routes/exit-frame.js
@@ -1,6 +1,11 @@
 import createApp from '@shopify/app-bridge';
 import {Redirect} from '@shopify/app-bridge/actions/index.js';
 
+const config = {
+  apiKey: "505015e6892ff21b6c433b09af1c7038",
+  host: new URLSearchParams(location.search).get("host"),
+  forceRedirect: true
+};
 const app = createApp(config);
 
 const redirect = Redirect.create(app);
