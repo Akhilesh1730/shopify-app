fcc53e8c2eb25c99b71bae4822131c6d9c96d54f
Akhilesh <dhoreakhilesh@gmail.com>
2025-06-13 03:50:36 +0530

    callback exrror added 59

--- app/routes/exit-frame.js
+++ app/routes/exit-frame.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useAppBridge } from "@shopify/app-bridge-react";
-import { Redirect } from "@shopify/app-bridge/actions";
+import { Redirect } from "@shopify/app-bridge/actions/index.js";
 import { useLocation } from "@remix-run/react";
 
 export default function MyRedirectComponent() {
